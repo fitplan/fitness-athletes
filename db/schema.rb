e9253935c641dd4169b0e81ff4ababb28d2b6ab9
Slav <slav@Vyacheslavs-MacBook-Air.local>
2015-11-24 16:14:21 -0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206061412) do
+ActiveRecord::Schema.define(version: 20151124233549) do
 
   create_table "authorizations", force: :cascade do |t|
     t.integer  "user_id",    null: false
