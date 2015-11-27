c920688a10d6d429a9cb822442d31196e981dc16
Slav <slav@Vyacheslavs-MacBook-Air.local>
2015-11-27 14:29:26 -0800

    Remove url validation

--- app/models/athlete/link.rb
+++ app/models/athlete/link.rb
@@ -1,14 +1,12 @@
 module Athlete
   class Link < Athlete::Base
-    validates :url, presence: true
     validates :instagram_url, presence: true
     validate :url_is_valid
 
     def url_is_valid
-      uri = URI.parse(url) || URI.parse(instagram_url)
+      uri = URI.parse(instagram_url)
       fail 'invalid URL' unless uri.scheme == 'https' || uri.scheme == 'http'
     rescue
-      errors.add :url, 'is invalid'
       errors.add :instagram_url, 'is invalid'
     end
   end
